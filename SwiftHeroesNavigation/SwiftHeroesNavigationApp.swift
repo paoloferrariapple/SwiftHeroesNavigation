1e3637188ff8f978c0060f3e855e22566f292b2d
Paolo Ferrari <paolo.ferrari@accenture.com>
2024-02-06 18:41:28 +0100

    Initial Commit

---
+++ SwiftHeroesNavigation/SwiftHeroesNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHeroesNavigationApp.swift
+//  SwiftHeroesNavigation
+//
+//  Created by paolo.ferrari on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHeroesNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
